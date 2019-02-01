69567d40071da0cf89fa7f736a71c27cf6e551d1
Ely <ely137137@gmail.com>
2019-02-01 23:48:33 +0300

    Сделал список объявлений

--- index.php
+++ index.php
@@ -109,17 +109,18 @@ $lot_list = [$lot1, $lot2, $lot3, $lot4, $lot5, $lot6];
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <!--заполните этот список из массива с товарами-->
+            <?php for($i = 0; $i < count($lot_list); $i++): ?>
+            <?php $current_lot = $lot_list[$i]; ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
+                    <img src="<?=$current_lot['url']; ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                    <span class="lot__category"><?=$current_lot['category']; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$current_lot['name']; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
+                            <span class="lot__amount"><?=$current_lot['price']; ?></span>
                             <span class="lot__cost">цена<b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
@@ -128,6 +129,7 @@ $lot_list = [$lot1, $lot2, $lot3, $lot4, $lot5, $lot6];
                     </div>
                 </div>
             </li>
+            <?php endfor; ?>
         </ul>
     </section>
 </main>
