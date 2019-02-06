e1f456adc966de2e345770949cc133fa10eae097
Ely <ely137137@gmail.com>
2019-02-07 00:09:01 +0300

    Перенес format_price()

--- functions.php
+++ functions.php
@@ -1,5 +1,22 @@
 <?php
     /**
+     *Функция для форматирования цены, а именно отделения разряда тысяч
+     *Пример: 14000 -> 14 000
+     *
+     * @param int $price
+     *
+     * @return string
+     */
+    function format_price ($price) {
+        $ceiled_price = ceil($price);
+        if ($ceiled_price > 1000) {
+            $ceiled_price = number_format($ceiled_price, 0, ',', ' ');
+        }
+
+        return $ceiled_price . ' ₽';
+    }
+
+    /**
      *Функция - шаблонизатор
      *
      *$name - имя файла - шаблона
