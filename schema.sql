f70eb25f7e50042c60ba9a8b237308008d702ce0
Ely <ely137137@gmail.com>
2019-02-10 18:31:48 +0300

    Поправил схему

--- schema.sql
+++ schema.sql
@@ -6,38 +6,42 @@ USE yeticave;
 
 CREATE TABLE category (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name CHAR
+  name VARCHAR(128) NOT NULL
 );
 
 CREATE TABLE lot (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  name CHAR,
-  category_id INT,
-  author_id INT,
-  description CHAR,
-  image TEXT,
+  time_of_create TIMESTAMP NOT NULL,
+  name VARCHAR(128) NOT NULL,
+  category_id INT NOT NULL,
+  author_id INT NOT NULL,
+  description TEXT(256) NOT NULL,
+  image VARCHAR(128) NOT NULL,
   start_cost INT NOT NULL,
-  final_date TIMESTAMP,
-  bet_step INT,
+  final_date TIMESTAMP NOT NULL,
+  bet_step INT NOT NULL,
   winner_id INT
 );
 
+CREATE INDEX lot_category_id ON lot(category_id);
+
 CREATE TABLE bet (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  bet_date TIMESTAMP,
-  amount_to_buy INT,
-  user_id INT,
-  lot_id INT
+  bet_date TIMESTAMP NOT NULL,
+  amount_to_buy INT NOT NULL,
+  user_id INT NOT NULL,
+  lot_id INT NOT NULL
 );
+
+CREATE INDEX bet_lot_id ON bet(lot_id);
+CREATE INDEX bet_date_create ON bet(bet_date);
 
 CREATE TABLE user (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  registrated_date DATE,
-  email CHAR NOT NULL UNIQUE,
-  name CHAR NOT NULL UNIQUE,
-  password CHAR NOT NULL UNIQUE,
-  avatar TEXT,
-  contact TEXT,
-  lot_id INT,
-  bet_id INT
+  registrated_date DATETIME NOT NULL,
+  email VARCHAR(128) NOT NULL UNIQUE,
+  name VARCHAR(128) NOT NULL UNIQUE,
+  password VARCHAR(128) NOT NULL UNIQUE,
+  avatar VARCHAR(128),
+  contact TEXT NOT NULL
 );
