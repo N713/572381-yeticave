b423cefd959ed200e7f8b087f2a39ad3fda3d267
Ely <ely137137@gmail.com>
2019-02-11 22:06:48 +0300

    Правки

--- schema.sql
+++ schema.sql
@@ -11,14 +11,14 @@ CREATE TABLE category (
 
 CREATE TABLE lot (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  time_of_create TIMESTAMP NOT NULL,
+  time_of_create DATETIME NOT NULL,
   name VARCHAR(128) NOT NULL,
   category_id INT NOT NULL,
   author_id INT NOT NULL,
-  description TEXT(256) NOT NULL,
+  description TEXT NOT NULL,
   image VARCHAR(128) NOT NULL,
   start_cost INT NOT NULL,
-  final_date TIMESTAMP NOT NULL,
+  final_date DATETIME NOT NULL,
   bet_step INT NOT NULL,
   winner_id INT
 );
@@ -27,7 +27,7 @@ CREATE INDEX lot_category_id ON lot(category_id);
 
 CREATE TABLE bet (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  bet_date TIMESTAMP NOT NULL,
+  bet_date DATETIME NOT NULL,
   amount_to_buy INT NOT NULL,
   user_id INT NOT NULL,
   lot_id INT NOT NULL
@@ -41,7 +41,7 @@ CREATE TABLE user (
   registrated_date DATETIME NOT NULL,
   email VARCHAR(128) NOT NULL UNIQUE,
   name VARCHAR(128) NOT NULL UNIQUE,
-  password VARCHAR(128) NOT NULL UNIQUE,
+  password VARCHAR(128) NOT NULL,
   avatar VARCHAR(128),
   contact VARCHAR(256) NOT NULL
 );
