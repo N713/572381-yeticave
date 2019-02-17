dc67a2fcd57e35f7e2f4154509a42f4fed605e86
Ely <ely137137@gmail.com>
2019-02-18 00:41:08 +0300

    Изменения в проверках

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $connect = mysqli_connect('localhost', 'root', '', 'yeticave' );
 
 if (!$connect) {
     print('Error: ' + mysqli_connect_error());
+    die;
 }
 
 mysqli_set_charset($connect, 'utf8');
@@ -13,11 +14,12 @@ mysqli_set_charset($connect, 'utf8');
 $sql = 'SELECT id, name FROM category';
 $result = mysqli_query($connect, $sql);
 
-if ($result) {
-    $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
-} else {
+if (!$result) {
     print (mysqli_error($connect));
+    die;
 }
+
+$categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 $sql = 'SELECT lot.id AS lot_id, time_of_create, lot.name AS lot_name, category_id, author_id, description,
             image, start_cost, final_date, bet_step, category.name AS category_name
@@ -33,6 +35,7 @@ if ($result) {
 }
 
 require_once('functions.php');
+require_once('mysql_helper.php');
 
 $page_content = include_template('index.php', [
     'lot_list' => $lot_list,
