36a34ae911424bd8c23ee53d8a6f5cd42449706b
Ely <ely137137@gmail.com>
2019-02-22 01:54:38 +0300

    Реализация добавления лота

--- add.php
+++ add.php
@@ -19,12 +19,42 @@ if (!$connect) {
 mysqli_set_charset($connect, 'utf8');
 
 require_once('functions.php');
+require_once('mysql_helper.php');
 
 $categories = get_categories($connect);
 
 $page_content = include_template('add.php', [
     'categories' => $categories,
 ]);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        $name = $_POST['name'];
+        $category_id = $_POST['category'];
+        $description = $_POST['description'];
+        $image = $_POST['image'];
+        $start_cost = $_POST['start_cost'];
+        $bet_step = $_POST['bet_step'];
+        $final_date = $_POST['final_date'];
+
+        $sql = 'INSERT INTO lot (time_of_create, name, category_id, author_id, description,
+                                 image, start_cost, bet_step, final_date)
+                VALUES (NOW(), ?, ?, 3, ?, ?, ?, ?, ?)';
+
+        $stmt = mysqli_prepare($connect, $sql);
+        mysqli_stmt_bind_param($stmt, 'sissiis', $name, $category_id, $description,
+                                $image, $start_cost, $bet_step, $final_date);
+        $res = mysqli_stmt_execute($stmt);
+
+        if ($res) {
+
+            $lot_id = mysqli_insert_id($connect);
+            header("Location: lot.php?tab=" . $lot_id);
+
+        } else {
+            print('Error');
+        }
+}
 
 $layout_content = include_template('add_layout.php', [
     'content' => $page_content,
--- templates/add.php
+++ templates/add.php
@@ -1,9 +1,9 @@
-<form class="form form--add-lot container form--invalid" action="https://echo.htmlacademy.ru" method="post"> <!-- form--invalid -->
+<form class="form form--add-lot container form--invalid" action="add.php" method="post"> <!-- form--invalid -->
   <h2>Добавление лота</h2>
   <div class="form__container-two">
     <div class="form__item form__item--invalid"> <!-- form__item--invalid -->
       <label for="lot-name">Наименование</label>
-      <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота" required>
+      <input id="lot-name" type="text" name="name" placeholder="Введите наименование лота" required>
       <span class="form__error">Введите наименование лота</span>
     </div>
     <div class="form__item">
@@ -21,7 +21,7 @@
   </div>
   <div class="form__item form__item--wide">
     <label for="message">Описание</label>
-    <textarea id="message" name="message" placeholder="Напишите описание лота" required></textarea>
+    <textarea id="message" name="description" placeholder="Напишите описание лота" required></textarea>
     <span class="form__error">Напишите описание лота</span>
   </div>
   <div class="form__item form__item--file"> <!-- form__item--uploaded -->
@@ -33,7 +33,7 @@
       </div>
     </div>
     <div class="form__input-file">
-      <input class="visually-hidden" type="file" id="photo2" value="">
+      <input class="visually-hidden" type="file" id="photo2" name="image" value="">
       <label for="photo2">
         <span>+ Добавить</span>
       </label>
@@ -42,17 +42,17 @@
   <div class="form__container-three">
     <div class="form__item form__item--small">
       <label for="lot-rate">Начальная цена</label>
-      <input id="lot-rate" type="number" name="lot-rate" placeholder="0" required>
+      <input id="lot-rate" type="number" name="start_cost" placeholder="0" required>
       <span class="form__error">Введите начальную цену</span>
     </div>
     <div class="form__item form__item--small">
       <label for="lot-step">Шаг ставки</label>
-      <input id="lot-step" type="number" name="lot-step" placeholder="0" required>
+      <input id="lot-step" type="number" name="bet_step" placeholder="0" required>
       <span class="form__error">Введите шаг ставки</span>
     </div>
     <div class="form__item">
       <label for="lot-date">Дата окончания торгов</label>
-      <input class="form__input-date" id="lot-date" type="date" name="lot-date" required>
+      <input class="form__input-date" id="lot-date" type="date" name="final_date" required>
       <span class="form__error">Введите дату завершения торгов</span>
     </div>
   </div>
