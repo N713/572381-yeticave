1f7490adfcdcccbec3fa6b81bc4cbbac75e74ffb
Ely <ely137137@gmail.com>
2019-02-24 22:44:39 +0300

    Внес исправления

--- templates/add.php
+++ templates/add.php
@@ -6,20 +6,24 @@
       <?php $name = $lot['name'] ?? ''; ?>
       <label for="lot-name">Наименование</label>
       <input id="lot-name" type="text" name="name" placeholder="Введите наименование лота" value="<?= $name; ?>">
-      <span class="form__error">Введите наименование лота</span>
+      <span class="form__error"><?= $errors['name'] ?? ''; ?></span>
     </div>
     <?php $classname = isset($errors['name']) ? "form__item--invalid" : ""; ?>
     <div class="form__item <?= $classname; ?>">
       <label for="category">Категория</label>
-      <select id="category" name="category">
-        <option>Выберите категорию</option>
-        <?php foreach ($categories as $category): ?>
-          <?php if(isset($category['name'])): ?>
-              <option value="<?= strip_tags($category['id']); ?>"><?= strip_tags($category['name']); ?></option>
-          <?php endif; ?>
-        <?php endforeach; ?>
-      </select>
-      <span class="form__error">Выберите категорию</span>
+        <select id="category" name="category">
+          <option value="">Выберите категорию</option>
+            <?php foreach ($categories as $category): ?>
+              <?php if (isset($category['name'], $category['id'])): ?>
+                  <option
+                      value="<?= strip_tags($category['id']); ?>"
+                      <?= isset($lot['category']) && $lot['category'] === $category['id'] ? ' selected' : ''; ?>>
+                      <?= strip_tags($category['name']); ?>
+                  </option>
+              <?php endif; ?>
+            <?php endforeach; ?>
+        </select>
+      <span class="form__error"><?= $errors['category'] ?? ''; ?></span>
     </div>
   </div>
   <?php $classname = isset($errors['description']) ? "form__item--invalid" : ""; ?>
@@ -27,7 +31,7 @@
     <?php $description = $lot['description'] ?? ''; ?>
     <label for="message">Описание</label>
     <textarea id="message" name="description" placeholder="Напишите описание лота"><?= $description; ?></textarea>
-    <span class="form__error">Напишите описание лота</span>
+    <span class="form__error"><?= $errors['description'] ?? ''; ?></span>
   </div>
   <?php $classname = isset($errors['image']) ? "form__item--invalid" : ""; ?>
   <div class="form__item form__item--file <?= $classname; ?>"> <!-- form__item--uploaded -->
@@ -45,6 +49,7 @@
         <span>+ Добавить</span>
       </label>
     </div>
+    <span class="form__error"><?= $errors['image'] ?? ''; ?></span>
   </div>
   <div class="form__container-three">
     <?php $classname = isset($errors['start_cost']) ? "form__item--invalid" : ""; ?>
@@ -52,28 +57,23 @@
       <?php $start_cost = $lot['start_cost'] ?? ''; ?>
       <label for="lot-rate">Начальная цена</label>
       <input id="lot-rate" type="number" name="start_cost" placeholder="0" value="<?= $start_cost; ?>">
-      <span class="form__error">Введите начальную цену</span>
+      <span class="form__error"><?= $errors['start_cost'] ?? ''; ?></span>
     </div>
     <?php $classname = isset($errors['bet_step']) ? "form__item--invalid" : ""; ?>
     <div class="form__item form__item--small <?= $classname; ?>">
       <?php $bet_step = $lot['bet_step'] ?? ''; ?>
       <label for="lot-step">Шаг ставки</label>
       <input id="lot-step" type="number" name="bet_step" placeholder="0" value="<?= $bet_step; ?>">
-      <span class="form__error">Введите шаг ставки</span>
+      <span class="form__error"><?= $errors['bet_step'] ?? ''; ?></span>
     </div>
     <?php $classname = isset($errors['final_date']) ? "form__item--invalid" : ""; ?>
     <div class="form__item <?= $classname; ?>">
       <?php $final_date = $lot['final_date'] ?? ''; ?>
       <label for="lot-date">Дата окончания торгов</label>
       <input class="form__input-date" id="lot-date" type="date" name="final_date" value="<?= $final_date; ?>">
-      <span class="form__error">Введите дату завершения торгов</span>
+      <span class="form__error"><?= $errors['final_date'] ?? ''; ?></span>
     </div>
   </div>
-  <?php if (count($errors) !== 0): ?>
-    <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
-    <?php foreach($errors as $error => $value): ?>
-      <span class="form__error form__error--bottom">Не заполнено поле: <?= $error[$value]; ?></span>
-    <?php endforeach; ?>
-  <?php endif; ?>
+  <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
   <button type="submit" class="button">Добавить лот</button>
 </form>
