0883e4d35b53a22201f5809395007bab27fd307c
Ely <ely137137@gmail.com>
2019-02-26 01:09:17 +0300

    Шаблон регистрации

--- templates/sign_up_layout.php
+++ templates/sign_up_layout.php
@@ -2,13 +2,7 @@
 <html lang="ru">
 <head>
   <meta charset="UTF-8">
-  <title>
-    <?php foreach($lot as $item): ?>
-        <?php if (isset($item['lot_name'])): ?>
-            <?= $item['lot_name']; ?>
-        <?php endif; ?>
-    <?php endforeach; ?>
-  </title>
+  <title>Регистрация</title>
   <link href="../css/normalize.min.css" rel="stylesheet">
   <link href="../css/style.css" rel="stylesheet">
 </head>
@@ -30,7 +24,7 @@
       <nav class="user-menu">
         <ul class="user-menu__list">
           <li class="user-menu__item">
-            <a href="sign_up.php">Регистрация</a>
+            <a href="#">Регистрация</a>
           </li>
           <li class="user-menu__item">
             <a href="login.html">Вход</a>
@@ -43,102 +37,16 @@
   <main>
     <nav class="nav">
       <ul class="nav__list container">
-        <?php foreach($categories as $category): ?>
-          <li class="nav__item">
-            <?php if (isset($category['name'])): ?>
-              <a href="all-lots.html"><?= strip_tags($category['name']); ?></a>
-            <?php endif; ?>
-          </li>
+        <?php foreach ($categories as $category): ?>
+            <li class="nav__item">
+                <?php if(isset($category['name'])): ?>
+                    <a href="pages/all-lots.html"><?= strip_tags($category['name']); ?></a>
+                <?php endif; ?>
+            </li>
         <?php endforeach; ?>
       </ul>
     </nav>
-    <section class="lot-item container">
-      <?= $content; ?>
-        <div class="lot-item__right">
-          <div class="lot-item__state">
-            <div class="lot-item__timer timer">
-              10:54
-            </div>
-            <div class="lot-item__cost-state">
-              <div class="lot-item__rate">
-                <span class="lot-item__amount">Текущая цена</span>
-                <?php foreach ($lot as $item): ?>
-                    <span class="lot-item__cost"><?= strip_tags($item['start_cost']); ?></span>
-                <?php endforeach; ?>
-              </div>
-              <!--
-              <div class="lot-item__min-cost">
-                Мин. ставка <span>12 000 р</span>
-              </div>
-            </div>
-            <form class="lot-item__form" action="https://echo.htmlacademy.ru" method="post">
-              <p class="lot-item__form-item form__item form__item--invalid">
-                <label for="cost">Ваша ставка</label>
-                <input id="cost" type="text" name="cost" placeholder="12 000">
-                <span class="form__error">Введите наименование лота</span>
-              </p>
-              <button type="submit" class="button">Сделать ставку</button>
-            </form>
-          </div>
-          <div class="history">
-            <h3>История ставок (<span>10</span>)</h3>
-            <table class="history__list">
-              <tr class="history__item">
-                <td class="history__name">Иван</td>
-                <td class="history__price">10 999 р</td>
-                <td class="history__time">5 минут назад</td>
-              </tr>
-              <tr class="history__item">
-                <td class="history__name">Константин</td>
-                <td class="history__price">10 999 р</td>
-                <td class="history__time">20 минут назад</td>
-              </tr>
-              <tr class="history__item">
-                <td class="history__name">Евгений</td>
-                <td class="history__price">10 999 р</td>
-                <td class="history__time">Час назад</td>
-              </tr>
-              <tr class="history__item">
-                <td class="history__name">Игорь</td>
-                <td class="history__price">10 999 р</td>
-                <td class="history__time">19.03.17 в 08:21</td>
-              </tr>
-              <tr class="history__item">
-                <td class="history__name">Енакентий</td>
-                <td class="history__price">10 999 р</td>
-                <td class="history__time">19.03.17 в 13:20</td>
-              </tr>
-              <tr class="history__item">
-                <td class="history__name">Семён</td>
-                <td class="history__price">10 999 р</td>
-                <td class="history__time">19.03.17 в 12:20</td>
-              </tr>
-              <tr class="history__item">
-                <td class="history__name">Илья</td>
-                <td class="history__price">10 999 р</td>
-                <td class="history__time">19.03.17 в 10:20</td>
-              </tr>
-              <tr class="history__item">
-                <td class="history__name">Енакентий</td>
-                <td class="history__price">10 999 р</td>
-                <td class="history__time">19.03.17 в 13:20</td>
-              </tr>
-              <tr class="history__item">
-                <td class="history__name">Семён</td>
-                <td class="history__price">10 999 р</td>
-                <td class="history__time">19.03.17 в 12:20</td>
-              </tr>
-              <tr class="history__item">
-                <td class="history__name">Илья</td>
-                <td class="history__price">10 999 р</td>
-                <td class="history__time">19.03.17 в 10:20</td>
-              </tr>
-            </table>
-          </div>
-        </div>
-      </div>
-    -->
-    </section>
+    <?= $content; ?>
   </main>
 
 </div>
@@ -146,13 +54,13 @@
 <footer class="main-footer">
   <nav class="nav">
     <ul class="nav__list container">
-      <?php foreach($categories as $category): ?>
+      <?php foreach ($categories as $category): ?>
           <li class="nav__item">
-            <?php if (isset($category['name'])): ?>
-              <a href="all-lots.html"><?= strip_tags($category['name']); ?></a>
-            <?php endif; ?>
+              <?php if(isset($category['name'])): ?>
+                  <a href="pages/all-lots.html"><?= strip_tags($category['name']); ?></a>
+              <?php endif; ?>
           </li>
-        <?php endforeach; ?>
+      <?php endforeach; ?>
     </ul>
   </nav>
   <div class="main-footer__bottom container">
